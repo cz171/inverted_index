c20d8e277c2e7890e3fbef603f874423a362d863
Theophilus Mensah <theomens@man.cs.rutgers.edu>
2013-12-26 15:41:42 -0500

    reduce done; sorting to be completed

--- mapred.c
+++ mapred.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/time.h>
 #include <omp.h>
 
 char **fileList;
@@ -85,13 +86,9 @@ void map(){
 	char *line = NULL;
   	size_t len = 0;
   	ssize_t read;
-  	char *tok;
-
-	Word_key *map_result;
+  	char *tok=NULL;
+	
 	my_job= malloc(2*sizeof(int));
-
-	/* allocate memory assuming 500 word each segment*/
-	map_result= malloc((num_args*(500))*sizeof(Word_key));
 
 	for(i=0;i < num_args-2;i++){
 	//printf("in mapping loop%d\n",num_args);
@@ -113,9 +110,10 @@ void map(){
 	while (tok != NULL){      	
 	//printf("in tokens loop,%s, %s\n",tok,currFile);
 	/* add word to result*/
-	map_result[count].word=tok;
-	map_result[count].docName=currFile;
-	tok = strtok (NULL, " ");
+	map_data[rank][count].word=tok;
+	map_data[rank][count].docName=currFile;
+	//printf("inserted word=> %s\n",map_data[rank][count].word);
+	tok = strtok (NULL," ");
 	/*advance iterator*/
         count++;
 	}
@@ -125,127 +123,186 @@ void map(){
 	
 	}
 	}
-	#pragma omp critical
-	{	
-	map_data[rank]=map_result;
-	//printf("rank %d added result to main\n",rank);	
-	}
 	
 	if(rank ==0){
 	  num_per_reducer= num_procs/2;
 	}
 
-	free(map_result);
 	#pragma omp barrier
 }
 
 void reduce(){
 	int a,i,j,k;
 	int my_rank= omp_get_thread_num();
-        int num_procs= omp_get_num_threads();
+      
 	comb_wordkey *reduce_result;
-	Word_key *map_result;
 
-	map_result= malloc((num_args*(500))*sizeof(Word_key));
-	reduce_result=malloc((num_per_reducer*num_args*(500))*sizeof(Word_key));
-
+	reduce_result=malloc((num_per_reducer*num_args*500)*sizeof(Word_key));
+	output=malloc((2*(num_per_reducer*num_args*500))*sizeof(Word_key));
+	
+	/*allocate memory for reduce_result*/
 	for(i=0;i<(num_per_reducer*num_args*(500));i++){
 		reduce_result[i].docName=malloc((num_args-2)*sizeof(char));
+		reduce_result[i].word=malloc(sizeof(char));
+		reduce_result[i].word=NULL;
+		for(j=0;j<(num_args-2);j++){
+			reduce_result[i].docName[j]=malloc(sizeof(char));
+			reduce_result[i].docName[j]=NULL;
+		}
 	}
 
-	/* combine maps */
+	/*allocate memory for output list*/
+        for(i=0;i<(num_per_reducer*num_args*(500));i++){
+                output[i].docName=malloc((num_args-2)*sizeof(char));
+                output[i].word=malloc(sizeof(char));
+                output[i].word=NULL;
+                for(j=0;j<(num_args-2);j++){
+                        output[i].docName[j]=malloc(sizeof(char));
+                        output[i].docName[j]=NULL;
+                }
+        }
+	
+	/* combine and merge 1st half of maps */
 	if(my_rank == 0){
 	for(i=0;i<num_per_reducer;i++){
-	printf("in map\n");
-	map_result=map_data[i];
-		for(j=0;j<(num_args*(500));j++){
-		if(map_result[j].word != NULL){
-		printf("in map-word\n");
-		//printf("map word %s\n",map_result[j].word);
+		for(j=0;j<((num_args-2)*500);j++){
+		if(map_data[i][j].word != NULL){
 			for(k=0;k<(num_args*(500));k++){	
-				if(reduce_result[k].word != NULL){
-				printf("in result-word\n");
-				//printf("reduce word %s\n",reduce_result[k].word);
-				if(strcmp(reduce_result[k].word , map_result[j].word) == 0){
-					/*for(a=0;a<num_args-2;a++){
-					if(reduce_result[k].docName[a] != NULL){
-					if(strcmp(reduce_result[k].docName[a],map_result[j].docName)==0)
-						break;// word and docname exists
-						printf("word+doc found\n");
-					}else{
-					reduce_result[k].docName[a]=map_result[j].docName;
+				if(output[k].word != NULL){
+
+				if(output[k].word == map_data[i][j].word){
+					for(a=0;a<num_args-2;a++){
+					if(output[k].docName[a] != NULL){
+						//printf("not null\n");
+					if(output[k].docName[a] == map_data[i][j].docName){
+						//printf("word+doc found\n");
 						break;
-						printf("added new docname\n");
 					}
-					}*/
+					}else{
+						output[k].docName[a]=map_data[i][j].docName;
+                                                //printf("added new docname\n");
+						break;
+					}
+					}
+
 				}
 
 				}else{
-					printf("in new word\n");
-					//printf("new word, %s, added\n",map_result[j].word);
-					reduce_result[k].word= map_result[j].word;
-					reduce_result[k].docName[0]= map_result[j].docName;
-				//	break;
-				
-				}	
+					//printf("in new word\n");                       
+					output[k].word= map_data[i][j].word;
+                        		output[k].docName[0]= map_data[i][j].docName;
+                        		break;
 
+
+				}
 			}
-
-		}else{
-			break;
-	}
+		}else break;
 	}
 	}
 	}
 
-/*
+	/* combine and merge second half of map*/
 	if(my_rank ==1){
-	for(i=num_per_reducer;i<num_procs;i++){
-	map_result=map_data[i];
-                for(j=0;j<sizeof(map_result);j++){
-                if(map_result[j].word != NULL){
-                        for(k=0;k<sizeof(comb_wordkey);k++){
+		//printf("rank 2");
+	for(i=num_per_reducer;i<num_per_reducer*2;i++){
+		for(j=0;j<((num_args-2)*500);j++){
+                if(map_data[i][j].word != NULL){
+                        for(k=0;k<(num_args*(500));k++){
                                 if(reduce_result[k].word != NULL){
-                                if(strcmp(reduce_result[k].word,map_result[j].word) == 0){
+
+                                if(reduce_result[k].word == map_data[i][j].word){
                                         for(a=0;a<num_args-2;a++){
                                         if(reduce_result[k].docName[a] != NULL){
-                                        if(strcmp(reduce_result[k].docName[a],map_result[j].docName)==0)
-                                                break;// word and docname exists
+                                                //printf("not null\n");
+                                        if(reduce_result[k].docName[a] == map_data[i][j].docName){
+                                                //printf("word+doc found\n");
+                                                break;
+                                        }
                                         }else{
-                                        reduce_result[k].docName[a]=map_result[j].docName;
+                                                reduce_result[k].docName[a]=map_data[i][j].docName;
+                                                //printf("added new docname\n");
                                                 break;
                                         }
                                         }
+
                                 }
 
                                 }else{
-                                        reduce_result[k].word= map_result[j].word;
-                                        reduce_result[k].docName[0]= map_result[j].docName;
-                                        //break;
+                                        //printf("in new word\n");
+                                        reduce_result[k].word= map_data[i][j].word;
+                                        reduce_result[k].docName[0]= map_data[i][j].docName;
+                                        break;
+
 
                                 }
-
                         }
+                }else break;
+        }
+        }
+	}
 
-                }else{
-                        break;
-        }
-        }
-	
-
-        }
-	}*/
 	#pragma omp barrier
 	
-	/* add to final list */
-		
+	/* combine two reduce lists*/
+	if(my_rank ==1){
+
+         for(i=0;i<(num_per_reducer*num_args*500);i++){
+                if(reduce_result[i].word != NULL){
+                        for(k=0;k<(2*num_per_reducer*num_args*500);k++){
+                                if(output[k].word != NULL){
+
+                                if(output[k].word == reduce_result[i].word){
+					int found[num_args-2];
+                                        for(a=0;a<num_args-2;a++){
+                                        if(output[k].docName[a] != NULL){
+                                                //printf("not null\n");
+
+					for(j=0;j<num_args-2;j++){
+                                        if(output[k].docName[a] == reduce_result[i].docName[j]){
+                                                //printf("word+doc found\n");
+						found[j]=1;
+                                                break;
+                                        
+                                        }
+
+                                        }
+                                        }
+					}
+						for(a=0;a<num_args-2;a++){
+                                                   if(output[k].docName[a] == NULL){
+							for(j=0;j<num_args-2;j++){
+							if(found[j] ==0){
+                                                	output[k].docName[a]=reduce_result[i].docName[j];
+                                                	//printf("added new docname\n");
+                                               		break;
+							}
+							}
+						}
+                                        }
+
+				}
+                                
+				
+                                }else{
+                                       // printf("in new word\n");
+                                        output[k].word= reduce_result[i].word;
+                                        output[k].docName = reduce_result[i].docName;
+                                        break;
+
+				}
+                                }
+           		             
+                }else break;
+        }
+        }		
 	
 	/*final sort*/
 	if(my_rank == 0){
 	//sort();
 
 	}
-
+	free(reduce_result);
+	#pragma omp barrier
 }
 
 void sort(){
@@ -282,11 +339,12 @@ void write_output_to_file(){
 	}
 	
 	}
+	fclose(fp);
 }
 
 int main(int argc, char *argv[]){
-
-	int i,thread_count,count=2;
+	long t_start, t_end;
+	int i,j,thread_count,count=2;
 	num_args= argc;
 	fileList= malloc((num_args-2)*sizeof(char));
 	/*check for filenames in cmd input*/	
@@ -300,20 +358,34 @@ int main(int argc, char *argv[]){
 		fileList[i] = argv[count];
 		count++;
 	}
-	thread_count = strtol(argv[1], NULL,10);	
+	thread_count = strtol(argv[1], NULL,10);
+	
+	/* allocate memory for per proces map result storage */	
 	map_data = malloc(thread_count*sizeof(Word_key));
 	for(i=0;i<thread_count;i++){
-	map_data[i]= malloc((num_args*(500))*sizeof(Word_key));
+	map_data[i]= malloc(((num_args-2)*(500))*sizeof(Word_key));
+		for(j=0;j<(num_args-2)*500;j++){
+		map_data[i][j].word= malloc(sizeof(char));
+		map_data[i][j].docName= malloc(sizeof(char));
+		map_data[i][j].word=NULL;
+		map_data[i][j].docName=NULL;
+		}
+
 	}
+
+	t_start = omp_get_wtime();
 	#pragma omp parallel num_threads(thread_count)
 	map();
 
 	#pragma omp parallel num_threads(2)
 	reduce();
 
-
-	//write_output_to_file();
-
+	t_end = omp_get_wtime();
+	
+	write_output_to_file();
+	free(output);
+	free(map_data);
+	printf("Compute time for %d threads =>%lf\n",thread_count,(t_end-t_start));
 	return 0;
 }
 
